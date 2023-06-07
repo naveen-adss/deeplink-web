01086d777ac1a361559cfcc596976449532e0dff
Naveen Vignesh <nvb.dev@adss.com>
2023-06-07 11:27:38 +0530

    feat: update labels

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express()
 
 app.use(express.static("public"))
 
-// app.get("/testscreen/:id?", (req, res) => {
-//     const id = req.params.id;
-//     res.send(`Got ID: ${id || 0}`);
-// })
+app.get("/testscreen/:id?", (req, res) => {
+    const id = req.params.id;
+    res.send(`Got Test ID: ${id || 0}`);
+})
+
+app.get("/products/:id?", (req, res) => {
+    const id = req.params.id;
+    res.send(`Got Product ID: ${id || 0}`);
+})
 
 app.listen(3000);
